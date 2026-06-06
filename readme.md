5551daf17ec3cc1e1424d4f10aad51be7ee69935
Cyberbee-pro <cyberbeepro@gmail.com>
2026-06-06 12:27:31 +0530

    added stuff

---
+++ readme.md
@@ -0,0 +1,30 @@
+# Macro board V1  
+
+## 3d model for reffernce : 
+<img src="render 2.jpg"/>
+
+## Wofi circuit render for reffernce : 
+<p align="center">
+   <img src="wofi render.png" alt="hackTime primary logo" width="340" />
+</p>
+---
+
+# Parts needed : 
+1. ESP 32
+2. 11 tactile buttons 
+3. 10 k-ohms potentiometer
+4. 3 Led Lights
+5. 1 joystick 
+6. 1 Breadboard or PCB BOARD
+
+---
+# Features
+<ul>
+<li>Knob control </li>
+<li>Mode switching </li>
+<li>9 programable Buttons</li>
+<li>Joystick mouce controll</li>
+<li>RT click (can be programed)</li>
+<li>Mode buld</li>
+
+</ul>
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,34 @@
 #include <Arduino.h>
+// Row 1 (Top)
+#define KEY_TOP_LEFT   21  // btn6
+#define KEY_TOP_MID    22  // btn7
+#define KEY_TOP_RIGHT  23  // btn1
+
+// Row 2 (Middle)
+#define KEY_MID_LEFT    5  // btn5
+#define KEY_MID_MID    18  // btn4
+#define KEY_MID_RIGHT  19  // btn8
+
+// Row 3 (Bottom)
+#define KEY_BOT_LEFT    4  // btn3
+#define KEY_BOT_MID    16  // btn2
+#define KEY_BOT_RIGHT  17  // btn9
+
+#define BUTTON_RIGHT 26
+#define BUTTON_LEFT  27    // Joystick push button 
+
+#define BUTTON_TOGGLE_MACRO 14
+
+#define LED_UNO 25
+#define LED_UNO 33
+#define LED_UNO 32
+
+#define POT_SIG 35
+
+#define JOY_HOR 35
+#define JOY_VERT 35
+
+
 
 // put function declarations here:
 int myFunction(int, int);
